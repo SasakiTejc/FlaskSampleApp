ed01fda3ca6ebfbb9df4dedbd446ebbb6689d6c6
sasaki <sasaki.tejc180574@gmail.com>
2023-01-23 19:55:27 +0900

    create static

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('Index.html')
+    return render_template('Index.html',title="Index with jinja",message="Jinjaテンプレート例")
+
+@app.route('/<id>/<password>')
+def index2(id,password):
+    msg = 'id: %s, password: %s' %(id,password)
+    return render_template('index.html',title="index whis jinja",message=msg)
 
 if __name__ == '__main__':
     app.debug = True
